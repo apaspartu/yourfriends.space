f4d16a2c4d8ad74147642453efee46e0c17b5ebc
Vladyslav Vintoniuk <spatiumfabri@gmail.com>
2022-06-29 20:36:17 +0300

    Add style

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import cloudinary.api
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
